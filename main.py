ccac9a151c38a74e28485c2dac2345889e2fe0e6
Faroukhamadi <faroukhamadi1@outlook.com>
2024-02-07 23:32:07 +0100

    inital loop

--- main.py
+++ main.py
@@ -7,3 +7,13 @@ with open('cb-test.txt', 'r') as file:
 
 # Print the first element and format the json output
 print(json.dumps(data[0], indent=4))
+
+item_name = data[0]['item_name']
+buyer_item_description = data[0]['buyer_item_description']
+buyer_price = data[0]['buyer_price']
+seller_item_description = data[0]['seller_item_description']
+seller_price = data[0]['seller_price']
+dialog = data[0]['dialog']
+
+for d in dialog:
+    print(d['speaker'], d['text'])
