5f87f8622a6fe4174796010a6c7e1e4839fa5dda
faroukhamadi <faroukhamadi1@outlook.com>
2024-02-12 22:02:24 +0100

    rename method param

--- models/reward.py
+++ models/reward.py
@@ -4,8 +4,8 @@ class RewardModel:
         self.seller_target_price = seller_target_price
         self.buyer_target_price = buyer_target_price
 
-    def prompt(self, prompt=None):
-        return prompt if prompt else "Have they reached a deal?"
+    def prompt(self, text=None):
+        return text if text else "Have they reached a deal?"
 
     def reward(self):
         return (self.deal_price - self.seller_target_price) / (self.buyer_target_price - self.seller_target_price)
@@ -14,3 +14,4 @@ class RewardModel:
 reward = RewardModel(None, None, None)
 
 print(reward.prompt())
+
